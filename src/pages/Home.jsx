3eca1a8b8af9cdeaf92e4ca2221fdf7cdcdcd4af
dennis adrian <adriangbetancur@gmail.com>
2021-02-05 09:35:25 -0400

    create home page look and add css config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Col, Row, Typography } from 'antd';
+import React from 'react';
 
+const { Title } = Typography;
 const Home = () => {
-    return (
-        <div>
-            Home Page
-        </div>
-    )
-}
+	return (
+		<>
+			<Row>
+				<Col span={12} offset={5} align='center'>
+					<Title level={3}>
+						Look at the side bar on your left and choose your role
+					</Title>
+				</Col>
+			</Row>
+		</>
+	);
+};
 
-export default Home
+export default Home;
